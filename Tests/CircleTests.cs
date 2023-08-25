c81d96427df8434c2c28e21f06dcb16bfaefc70d
Максим <kyngfy-panda@yandex.ru>
2023-08-25 17:33:00 +0400

    Add circles and triangles tests

--- Tests/CircleTests.cs
+++ Tests/CircleTests.cs
@@ -1,11 +1,53 @@
-namespace Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace Tests;
 
-        }
+using Core;
+
+public class CircleTests
+{
+    // https://en.wikipedia.org/wiki/Area_of_a_circle
+    private static double CircleArea(double radius) => radius * radius * Math.PI;
+
+    [Fact]
+    public void TestAreaSimple()
+    {
+        double radius = 5.0;
+        var circle = new Circle(radius);
+
+        double expected = CircleArea(radius);
+        double actual = circle.Area;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TestAreaWithOneRadius()
+    {
+        double radius = 1.0;
+        var circle = new Circle(radius);
+
+        double expected = Math.PI;
+        double actual = circle.Area;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TestNegativeInitialization()
+    {
+        double radius = -10.0;
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(radius));
+    }
+
+    [Fact]
+    public void TestZeroArea()
+    {
+        double radius = 0.0;
+        var circle = new Circle(radius);
+
+        double expected = 0.0;
+        double actual = circle.Area;
+
+        Assert.Equal(expected, actual);
     }
 }
--- Tests/TriangleTests.cs
+++ Tests/TriangleTests.cs
@@ -1,10 +1,107 @@
 namespace Tests;
 
+using Core;
+
 public class TriangleTests
 {
-    [Fact]
-    public void Test1()
+    // https://en.wikipedia.org/wiki/Heron%27s_formula
+    private static double TriangleArea(double a, double b, double c)
     {
+        double s = (a + b + c) / 2.0; // semiperimeter
+        double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        return area;
+    }
 
+    [Fact]
+    public void TestZeroArea()
+    {
+        double a = 0.0, b = 0.0, c = 0.0;
+        var triangle = new Triangle(a, b, c);
+
+        double expected = 0.0;
+        double actual = triangle.Area;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TestAreaDegenerated()
+    {
+        double a = 2.0, b = 5.0, c = 7.0;
+        var triangle = new Triangle(a, b, c);
+
+        double expected = 0.0;
+        double actual = triangle.Area;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TestAreaRight()
+    {
+        double a = 3.0, b = 4.0, c = 5.0;
+        var triangle = new Triangle(a, b, c);
+
+        double expected = 6.0;
+        double actual = triangle.Area;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TestAreaEquilateral()
+    {
+        double a = 3.0, b = 3.0, c = 3.0;
+        var triangle = new Triangle(a, b, c);
+
+        double expected = TriangleArea(a, b, c);
+        double actual = triangle.Area;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TestAreaIsosceles()
+    {
+        double a = 3.0, b = 3.0, c = 5.0;
+        var triangle = new Triangle(a, b, c);
+
+        double expected = TriangleArea(a, b, c);
+        double actual = triangle.Area;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TestAreaDifferentSidesInitializationOrder()
+    {
+        double a = 5.0, b = 6.0, c = 7.0;
+        double[][] sides =
+            { new[] { a, b, c }, new[] { a, c, b }, new[] { b, a, c }, new[] { b, c, a }, new[] { c, a, b }, new[] { c, b, a } };
+
+        double expected = TriangleArea(a, b, c);
+
+        foreach (double[] data in sides)
+        {
+            var triangle = new Triangle(data[0], data[1], data[2]);
+            double actual = triangle.Area;
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    [Fact]
+    public void TestNegativeInitialization()
+    {
+        double a = -5.0, b = 6.0, c = -7.0;
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(a, b, c));
+    }
+
+    [Fact] // https://en.wikipedia.org/wiki/Triangle_inequality
+    public void TestInequality()
+    {
+        double a = 1.0, b = 2.0, c = 10.0;
+
+        Assert.Throws<ArgumentException>(() => new Triangle(a, b, c));
     }
 }
